c6bf157ab84f7facf31f4466460db3f620420c85
aLimonad <46812712+aLimonad@users.noreply.github.com>
2025-02-18 17:15:07 +0300

    Update panel_api.py

--- utils/panel_api.py
+++ utils/panel_api.py
@@ -322,10 +322,10 @@ async def send_notify_request(panel_data: PanelType, username: UserType) -> None
         ValueError: If the function fails to disable the user on both the HTTP
         and HTTPS endpoints.
     """
-	logger.info("Test log");
+	logger.info("Test log")
     try:
-	logger.info(username.name);
-	logger.info(data["TIME_TO_ACTIVE_USERS"]);
+	logger.info(username.name)
+	logger.info(data["TIME_TO_ACTIVE_USERS"])
         if panel_data.panel_notify_point is not None and panel_data.panel_notify_point != "":
             data = await read_config()
             payload = {
